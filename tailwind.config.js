c45ddca04e3b33d72b7e21be902f4ffa44d48077
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-08-06 21:32:43 -0400

    style: color palette

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#00171fff",
+        base: "#003459ff",
+        secondary: "#006ea1ff",
+        primary: "#00a8e8ff",
+        light: "#e8f7fdff",
+      },
+    },
+  },
+  plugins: [],
+};
